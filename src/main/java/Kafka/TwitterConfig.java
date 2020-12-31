5f065613f050343b36c4c495bcb7f323c08f9a08
pedrofig <pfbddf@gmail.com>
2020-12-31 14:43:41 +0000

    First Commit

---
+++ src/main/java/Kafka/TwitterConfig.java
@@ -0,0 +1,8 @@
+package Kafka;
+
+public class TwitterConfig {
+    public static final String CONSUMER_KEYS= "23L6P7zp3UpXtTjzMKzgPd3zh";
+    public static final String CONSUMER_SECRETS= "cko3oybYt4ql0MMt9nQB9YROs70MpEFdG6yEUaKBXriJOeytMJ";
+    public static final String SECRET = "n2UgZP694y1BgBjbfhH25lJ3iTUytRXY6Qlqwoi9LXOGS"; //accessToken
+    public static final String TOKEN = "1303790168-8hYAb3F1S4c0zAcMa8vKFdzWxkv8HTGP4VSTei9"; //accessTokenSecret
+}
